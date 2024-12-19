f1244b5c5414fe2a5f55ff254331b9ce57704076
Beenish <beenishali693@gmail.com>
2024-12-19 00:42:50 -0500

    finished wave 4 (connecting task list front end to back end and updating app.jsx so that what we do on the front end is reflected in the back end and vice versa for get, patch, delete requests

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,100 @@
 import TaskList from './components/TaskList.jsx';
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
-const TASKS = [
-  {
-    id: 1,
-    title: 'Mow the lawn',
-    isComplete: false,
-  },
-  {
-    id: 2,
-    title: 'Cook Pasta',
-    isComplete: true,
-  },
-];
+// const TASKS = [
+//   {
+//     id: 1,
+//     title: 'Mow the lawn',
+//     isComplete: false,
+//   },
+//   {
+//     id: 2,
+//     title: 'Cook Pasta',
+//     isComplete: true,
+//   },
+// ];
 
-const App = () => {
-  const [taskData, setTaskData] = useState(TASKS);
+const taskAPIUrl = 'http://127.0.0.1:5000';
 
-  const handleTaskToggled = (id) => {
-    setTaskData(taskData => taskData.map(task => {
-      if (task.id === id) {
-        return {...task, isComplete: !task.isComplete};
-      }else {
-        return task;
-      }
-    }));
+const convertFromApi = (apiTask) => {
+  const newTask = {
+    ...apiTask,
+    isComplete: apiTask.is_complete
+  };
+  delete newTask.is_complete;
+  return newTask;
+};
+
+const getAllTasksApi = () => {
+  return axios.get(`${taskAPIUrl}/tasks`)
+    .then( response => {
+      const apiTasks = response.data;
+      const newTasks = apiTasks.map(convertFromApi);
+      return newTasks;
+    })
+    .catch(error => {
+      console.log(error);
+    });
+};
+
+
+const toggleCompletionApi = (id, taskData) => {
+  const testing = taskData.find(task => task.id === id);
+  const apiCall = testing.isComplete ? 'mark_incomplete' : 'mark_complete';
+  return axios.patch(`${taskAPIUrl}/tasks/${id}/${apiCall}`)
+    .then( response => {
+      const newTask = convertFromApi(response.data.task);
+      return newTask;
+    })
+    .catch( error => {
+      console.log(error);
+    });
+};
+
+const unregisterTaskApi = (id) => {
+  return axios.delete(`${taskAPIUrl}/tasks/${id}`)
+    .catch(error => {
+      console.log(error);
+    });
+};
+
+function App () {
+  const [taskData, setTaskData] = useState([]);
+
+  const getAllTasks = () => {
+    getAllTasksApi()
+      .then(tasks => {
+        setTaskData(tasks);
+      });
   };
 
+  useEffect(() => {
+    getAllTasks();
+  }, []);
+
+  const handleTaskToggled = (id) => {
+    toggleCompletionApi(id, taskData)
+      .then(newTask => {
+        setTaskData(taskData => taskData.map(task => {
+          if (task.id === id) {
+            return newTask;
+          } else {
+            return task;
+          }
+        }));
+      });
+  };
+  // };
+
   const handleUnregisterTask = (id) => {
-    setTaskData(taskData => taskData.filter(task => {
-      return task.id !== id;
-    }));
+    unregisterTaskApi(id)
+      .then(() => {
+        setTaskData(taskData => taskData.filter(task => {
+          return task.id !== id;
+        }));
+      });
   };
 
   return (
@@ -42,9 +105,9 @@ const App = () => {
       <main>
         <div>
           <TaskList
-          tasks = {taskData}
-          onTaskToggled = {handleTaskToggled}
-          onUnregisterTask={handleUnregisterTask}
+            tasks = {taskData}
+            onTaskToggled = {handleTaskToggled}
+            onUnregisterTask={handleUnregisterTask}
           />
         </div>
       </main>
